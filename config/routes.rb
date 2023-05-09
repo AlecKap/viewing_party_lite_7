0b353255649ee0a051e733ce7a7aed8eaf7182c1
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-09 16:57:57 -0600

    route: Add resources for users and discover_movies.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  resources :users, only: [:show, :new, :create] do
+    resources :discover_movies, only: [:index], controller: 'users/discover_movies'
+  end
 end
