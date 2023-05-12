25e672473b20dc64a180f903df54cd65b1faa780
Reid Miller <reid.s.miller@gmail.com>
2023-05-11 21:05:58 -0600

    test/update: Add movie_details, movie_cast, and movie_reviews endpoints to movie_service

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -33,20 +33,54 @@ RSpec.describe MovieService do
     end
 
     context '#movie_details', :vcr do
-      xit 'returns movie data' do
+      it 'returns movie data' do
         search = MovieService.new.movie_details('12444')
         expect(search).to be_a(Hash)
+
+        expect(search).to have_key(:title)
+        expect(search[:title]).to be_a(String)
+
+        expect(search).to have_key(:vote_average)
+        expect(search[:vote_average]).to be_a(Float)
+
+        expect(search).to have_key(:runtime)
+        expect(search[:runtime]).to be_a(Integer)
+
+        expect(search).to have_key(:genres)
+        expect(search[:genres]).to be_a(Array)
+
+        expect(search).to have_key(:overview)
+        expect(search[:overview]).to be_a(String)
+      end
+    end
+
+    context '#movie_cast', :vcr do
+      it 'returns movie data' do
+        search = MovieService.new.movie_cast('12444')
+        expect(search).to be_a(Hash)
+        expect(search[:cast]).to be_an(Array)
+        cast_data = search[:cast].first
+
+        expect(cast_data).to have_key(:name)
+        expect(cast_data[:name]).to be_a(String)
+
+        expect(cast_data).to have_key(:character)
+        expect(cast_data[:character]).to be_a(String)
+      end
+    end
+
+    context '#movie_reviews_details', :vcr do
+      it 'returns movie data' do
+        search = MovieService.new.movie_reviews_details('12444')
+        expect(search).to be_a(Hash)
+
+        expect(search[:total_results]).to be_an(Integer)
+
         expect(search[:results]).to be_an(Array)
-        movie_data = search[:results].first
+        review_data = search[:results].first
 
-        expect(movie_data).to have_key(:title)
-        expect(movie_data[:title]).to be_a(String)
-
-        expect(movie_data).to have_key(:vote_average)
-        expect(movie_data[:vote_average]).to be_a(Float)
-
-        expect(movie_data).to have_key(:runtime)
-        expect(movie_data[:runtime]).to be_a(Integer)
+        expect(review_data).to have_key(:content)
+        expect(review_data[:content]).to be_a(String)
       end
     end
   end
