087792521975d2660916ff12035ce4dbb73d9919
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-11 19:04:31 -0600

    feat: Create Movie class.

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,13 @@
 class Movie
   attr_reader :title,
               :vote_average,
-              :id
+              :id,
+              :summary
 
   def initialize(attributes)
+    @id = attributes[:id]
     @title = attributes[:title]
     @vote_average = attributes[:vote_average]
-    @id = attributes[:id]
+    @summary = attributes[:overview]
   end
 end
