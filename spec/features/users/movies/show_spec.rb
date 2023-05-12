8ab96ded63d39a0d25a92a4d4133bfb4ab869b66
Reid Miller <reid.s.miller@gmail.com>
2023-05-11 20:22:27 -0600

    fix: Fix tests and html for movie details page

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -5,40 +5,34 @@ RSpec.describe 'Movie Details Page', type: :feature do
     before(:each) do
       @user1 = User.create!(name: 'Rebecca Black', email: 'rebecca.black@gmail.com')
       visit user_discover_index_path(@user1)
-
-      click_link 'Find Top Rated Movies'
-
-      link = all('.title')[0]
-
-      link.click
+      fill_in 'search', with: 'Harry Potter and the Goblet of Fire'
+      click_button 'Find Movies'
+      click_link 'Harry Potter and the Goblet of Fire'
     end
 
-    it 'has a Discover Page button' do
-      expect(page).to have_button('Discover Page')
-
-      click_button 'Discover Page'
-
-      expect(current_path).to eq(user_discover_index_path(@user1))
-    end
-
-    it 'has a Create Viewing Party button' do
-      expect(page).to have_css('.create_viewing_party')
-
-      link = find('.create_viewing_party')
-
-      link.click
-
-      expect(page).to have_content('Viewing Parties')
-    end
-
-    it 'has movie details' do
-      expect(page).to have_css('.title')
-      expect(page).to have_css('.vote_average')
-      expect(page).to have_css('.runtime')
-      expect(page).to have_css('.genre')
-      expect(page).to have_css('.summary')
-      expect(page).to have_css('.cast')
-      expect(page).to have_css('.reviews')
+    describe 'movie details page', :vcr do
+      it 'has a Discover Page button' do
+        expect(page).to have_button('Discover Page')
+  
+        click_button 'Discover Page'
+  
+        expect(current_path).to eq(user_discover_index_path(@user1))
+      end
+  
+      it 'has a Create Viewing Party button' do
+        expect(page).to have_button('Create Viewing Party for Harry Potter and the Goblet of Fire')
+  
+      end
+  
+      it 'has movie details' do
+        expect(page).to have_css('.title')
+        expect(page).to have_css('.vote_average')
+        expect(page).to have_css('.runtime')
+        expect(page).to have_css('.genre')
+        expect(page).to have_css('.summary')
+        expect(page).to have_css('.cast')
+        expect(page).to have_css('.reviews')
+      end
     end
   end
 end
