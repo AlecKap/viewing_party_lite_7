99be58dca6ed9e394993b1be6a8d99e2ba6cddcd
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-12 17:22:36 -0600

    feat: Add host_check method.

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,12 @@
 class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
   has_many :users, through: :user_viewing_parties
+
+  def host_check(user_id)
+    if self.host == user_id
+      'The Host'
+    else
+      'Invited'
+    end
+  end
 end
