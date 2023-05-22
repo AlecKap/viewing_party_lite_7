8df8ede88ca6dcf27cafc56234558b3d9ebcf64a
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-22 16:46:45 -0600

    Authentication user story 2.

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'user registration page', type: :feature do
   describe 'As a user when I visit the /register path' do
-    it 'I see a form to input new user information' do
+    before :each do
       visit register_path
-
+    end
+    it 'I see a form to input new user information' do
       expect(page).to have_content('Register New User')
       expect(page).to have_content('Name:')
       expect(page).to have_field('user[name]')
@@ -28,8 +29,6 @@ RSpec.describe 'user registration page', type: :feature do
                             password: 'password123',
                             password_confirmation: 'password123')
 
-      visit register_path
-
       fill_in 'user[name]', with: 'Rebecca Black'
       fill_in 'user[email]', with: 'rebecca.black@gmail.com'
       fill_in 'Password:', with: 'password123'
@@ -38,6 +37,39 @@ RSpec.describe 'user registration page', type: :feature do
 
       expect(current_path).to eq(register_path)
       expect(page).to have_content('Email has already been taken')
+    end
+
+    it 'cannot leave password blank' do
+      fill_in 'user[name]', with: 'Rebecca Black'
+      fill_in 'user[email]', with: 'rebecca.black@gmail.com'
+      fill_in 'Password:', with: ''
+      fill_in 'Password Confirmation:', with: 'password123'
+      click_button 'Register User'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    it 'cannot leave password confirmation blank' do
+      fill_in 'user[name]', with: 'Rebecca Black'
+      fill_in 'user[email]', with: 'rebecca.black@gmail.com'
+      fill_in 'Password:', with: 'password123'
+      fill_in 'Password Confirmation:', with: ''
+      click_button 'Register User'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
+    it 'password and password confirmation must match' do
+      fill_in 'user[name]', with: 'Rebecca Black'
+      fill_in 'user[email]', with: 'rebecca.black@gmail.com'
+      fill_in 'Password:', with: 'password123'
+      fill_in 'Password Confirmation:', with: 'Password123'
+      click_button 'Register User'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Password confirmation doesn't match Password")
     end
   end
 end
